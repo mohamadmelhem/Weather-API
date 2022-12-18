f572999c71ec21a3c48ac7cab00b68be198cc1b2
mohamadmelhem <mhamadmlhm0@gmail.com>
2022-12-18 16:03:27 +0200

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
-
-import SayHi, { SayHello } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
-
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: "Mohamad & Ahmad"
-    };
+
   }
 
-  handleInputChange = value => {
-    this.setState({ name: value });
-  };
-
   render() {
-    return (
-      <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+    return (<div className="app">
+      <div className="navbar">
+        <input type="search" placeholder="type a city name" />
+        <button >Find weather</button>
       </div>
-    );
+
+      <div className="main">
+        <div className="live">
+          <img src="http://placekitten.com/200/200"/>
+            <p className="details">Temperature <span>10&deg;</span><span> to </span><span>11&deg;C</span></p>
+          <p className="more">hmidtly <span> 78% </span>pessure <span> 100<sup>8</sup>4<sup>8 </sup></span></p>
+        </div>
+        <div className="all">
+          <div className="box">
+            <span>03:00</span>
+            <img src="http://placekitten.com/100/100" />
+            <span>11&deg;C</span>
+          </div>
+          <div className="box">
+            <span>03:00</span>
+            <img src="http://placekitten.com/100/100" />
+            <span>11&deg;C</span>
+          </div>
+          <div className="box">
+            <span>03:00</span>
+            <img src="http://placekitten.com/100/100" />
+            <span>11&deg;C</span>
+          </div>
+          <div className="box">
+            <span>03:00</span>
+            <img src="http://placekitten.com/100/100" />
+            <span>11&deg;C</span>
+          </div>
+          <div className="box">
+            <span>03:00</span>
+            <img src="http://placekitten.com/100/100" />
+            <span>11&deg;C</span>
+          </div>
+          <div className="box">
+            <span>03:00</span>
+            <img src="http://placekitten.com/100/100" />
+            <span>11&deg;C</span>
+          </div>
+          <div className="box">
+            <span>03:00</span>
+            <img src="http://placekitten.com/100/100" />
+            <span>11&deg;C</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    )
   }
 }
 

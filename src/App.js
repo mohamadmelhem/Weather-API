e833f3010f51549fe0b4dbb44653eea0675f4034
mohamadmelhem <mhamadmlhm0@gmail.com>
2022-12-19 21:16:13 +0200

    edit Weather API: Extracting the values from the Fake Weather json

--- src/App.js
+++ src/App.js
@@ -2,42 +2,34 @@ import React, { Component } from "react";
 import Search from "./components/Search";
 import WeatherItem from "./components/WeatherItem";
 import WeatherDetails from "./components/WeatherDetails";
-import FakeWeather from "./Data/fakeWeatherData.json"
+import FakeWeather from "./Data/FakeWeatherData.json"
 import "./App.css";
-FakeWeather.list.lengh=24
+FakeWeather.list.lengh = 24
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state={
+  constructor(props) {
+    super(props);
+    this.state = {
+      data:{}
+    }
+  }
 
-  //   }
+  // componentDidMount() {
+  //   fetch('http://api.openweathermap.org/data/2.5/forecast?q=$london&cnt=8&units=metric&appid=$55229a9d8ce6b656375eedca5a3be0ca')
+  //     .then(function (response) {
+  //       // handle the response
+  //       console.log(response)
+  //     })
   // }
-  
-
   render() {
-
-    // function timeArray(FakeWeather) {
-    //   let time= []
-    //   for (let i = 5; i <= 11; i++) {
-    //     time.push(FakeWeather.list[i].dt_txt.slice(11,17));
-
-    //   }
-     
-    //   return time;
-    // }
-    // function weatherArray(FakeWeather){
-    //   let weatherA= []
-    //   for (let i=5; i <=11; i++){
-    //     weatherA.push(FakeWeather.list[i].temp)
-    //   }
-    //   return weatherA
-    // }
+    // .catch(function() {
+    //   // handle the error
+    // });
     return (<div className="app">
       <Search />
-      
+
       <div className="main">
-        <WeatherItem mainData={FakeWeather.list[0]}/>
-        <WeatherDetails data={FakeWeather}/>
+        <WeatherItem mainData={FakeWeather.list[0]} />
+        <WeatherDetails data={FakeWeather} />
       </div>
     </div>
 
